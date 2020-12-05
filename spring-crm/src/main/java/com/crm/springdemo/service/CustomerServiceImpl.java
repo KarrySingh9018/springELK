cd781b222d176054c720b41e671b19d939e314bf
Ramanpreet Singh <57689321+KarrySingh9018@users.noreply.github.com>
2020-12-05 18:47:26 -0500

    updated CustomerServiceImpl.java

--- spring-crm/src/main/java/com/crm/springdemo/service/CustomerServiceImpl.java
+++ spring-crm/src/main/java/com/crm/springdemo/service/CustomerServiceImpl.java
@@ -1,3 +1,6 @@
+/**
+* @author Ramanpreet Singh
+*/
 package com.crm.springdemo.service;
 
 import java.util.List;
@@ -9,6 +12,9 @@ import org.springframework.transaction.annotation.Transactional;
 import com.crm.springdemo.dao.CustomerDAO;
 import com.crm.springdemo.entity.Customer;
 
+/* Class containing the required business logic */
+/* @Transactional - any failure causes the entire operation to roll back to its previous state 
+and to re-throw the original exception. */
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
@@ -16,26 +22,39 @@ public class CustomerServiceImpl implements CustomerService {
 	@Autowired
 	private CustomerDAO customerDAO;
 	
+	/**
+	* @return list of customers
+	*/
 	@Override
-	@Transactional
+	@Transactional 
 	public List<Customer> getCustomers() {
 		return customerDAO.getCustomers();
 	}
-
+	/**
+	* @param theCustomer
+	* Method saves the customer entry in the repository
+	*/
 	@Override
 	@Transactional
 	public void saveCustomer(Customer theCustomer) {
 
 		customerDAO.saveCustomer(theCustomer);
 	}
-
+	/**
+	* @return Customer
+	* @param id
+	* Method returns the particular customer based on provided id
+	*/
 	@Override
 	@Transactional
 	public Customer getCustomer(int theId) {
 		
 		return customerDAO.getCustomer(theId);
 	}
-
+	/**
+	* @param theId
+	* Method deletes the customer in the repository for the given id
+	*/
 	@Override
 	@Transactional
 	public void deleteCustomer(int theId) {
